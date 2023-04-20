78319cf2947b861d1cd355c80bc3f9c1ff0038d1
qouillyan <stojakovicdragan98@gmail.com>
2023-04-20 22:22:30 +0200

    loading

--- src/app/components/AddOMDbMovieComponent.tsx
+++ src/app/components/AddOMDbMovieComponent.tsx
@@ -7,18 +7,24 @@ import { notficationManager } from 'app/utils/NotificationManager';
 import { AxiosError, AxiosResponse } from 'axios';
 import { IError } from 'app/types/IError';
 import { isObjOfType } from 'app/utils/typeCheckers';
+import { useContext } from 'react';
+import { LoadingContext } from 'app/context/LoadingContext';
 
 type Props = {
   omdbMovie: IMovieOMdb;
 };
 
 export const AddOMDbMovieComponent = ({ omdbMovie }: Props) => {
+  const { loading, setLoading } = useContext(LoadingContext);
+
   const { mutate } = useMutation(moviesService.CreateMovie, {
     onSuccess: (data: AxiosResponse<IMovie>) => {
+      setLoading(false);
       if (isObjOfType<IMovie>(data))
         notficationManager.success('Succesfully added a new movie!');
     },
     onError: (error: AxiosError<IError>) => {
+      setLoading(false);
       error.response?.data?.errors.forEach((error) =>
         notficationManager.error(error.msg)
       );
@@ -26,6 +32,7 @@ export const AddOMDbMovieComponent = ({ omdbMovie }: Props) => {
   });
 
   async function postMovie(movie: IMovieDraft) {
+    setLoading(true);
     mutate(movie);
   }
 
@@ -33,6 +40,7 @@ export const AddOMDbMovieComponent = ({ omdbMovie }: Props) => {
     <Box mt={3} position="absolute" bottom="0px">
       <Button
         color="inherit"
+        disabled={loading}
         onClick={() =>
           postMovie({
             title: omdbMovie.title,
